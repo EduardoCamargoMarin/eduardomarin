cbe56960232d4c4aba2aec1755b47e3497f6bdd8
Eduardo Marin <du.marin10@gmail.com>
2023-03-26 19:33:02 -0300

    1.0.0

--- code/EVA/evaMainFunction.js
+++ code/EVA/evaMainFunction.js
@@ -1,8 +1,46 @@
 
 const mainAudio = document.getElementById('evaMainAudio')
 const evaOnline = document.getElementById('evaOnline')
-
-
-evaOnline.addEventListener("click", () => {
+const meetAudio = document.getElementById('evaMeetAudio')
+// EVA´s activation
+evaOnline.addEventListener("load", () => {
     mainAudio.play()
 })
+//EVA welcome first meeting
+evaOnline.addEventListener("click", () => {
+    meetAudio.play()
+})
+
+//EVA Shutting down
+const shutdownAudio = document.getElementById('shutdownAudio')
+const evaAnswerShutDown = document.getElementById('shutdown')
+
+evaAnswerShutDown.addEventListener("click", () => {
+    shutdownAudio.play()
+})
+
+//Questions - Who are you?
+const whoAmIAudio = document.getElementById('whoAmIAudio')
+const evaAnswerWhoAmI = document.getElementById('whoAmI')
+
+evaAnswerWhoAmI.addEventListener("click", () => {
+    whoAmIAudio.play()
+})
+
+//Questions - Who is your creator?
+const creatorAudio = document.getElementById('creatorAudio')
+const evaAnswerCreator = document.getElementById('creator')
+
+evaAnswerCreator.addEventListener("click", () => {
+    creatorAudio.play()
+})
+//Questions - Website Tour
+const tourAudio = document.getElementById('tourAudio')
+const evaAnswerTour = document.getElementById('tour')
+
+evaAnswerTour.addEventListener("click", () => {
+    tourAudio.play()
+})
+
+
+
