10544310d68c455c2d9f292f4ee078b73757b12c
Eduardo Marin <du.marin10@gmail.com>
2023-03-26 03:27:13 -0300

    1.0.0

---
+++ code/EVA/evaMainFunction.js
@@ -0,0 +1,8 @@
+
+const mainAudio = document.getElementById('evaMainAudio')
+const evaOnline = document.getElementById('evaOnline')
+
+
+evaOnline.addEventListener("click", () => {
+    mainAudio.play()
+})
