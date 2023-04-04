fbe3e8e89dec0f2305bc5c9dfeb55c42547493e2
Eduardo Marin <du.marin10@gmail.com>
2023-04-03 21:39:04 -0300

    1.0.0

---
+++ code/sisters.js
@@ -0,0 +1,42 @@
+
+const evaAudio = document.getElementById('evaAudio')
+const eva = document.getElementById('eva')
+
+eva.addEventListener("click", () => {
+    evaAudio.play()
+})
+
+const farahAudio = document.getElementById('farahAudio')
+const farah = document.getElementById('farah')
+
+farah.addEventListener("click", () => {
+    farahAudio.play()
+})
+
+const saintAudio = document.getElementById('saintAudio')
+const saint = document.getElementById('saint')
+
+saint.addEventListener("click", () => {
+    saintAudio.play()
+})
+
+const evelineAudio = document.getElementById('evelineAudio')
+const eveline = document.getElementById('eveline')
+
+eveline.addEventListener("click", () => {
+    evelineAudio.play()
+})
+
+const lunaAudio = document.getElementById('lunaAudio')
+const luna = document.getElementById('luna')
+
+luna.addEventListener("click", () => {
+    lunaAudio.play()
+})
+
+const laylaAudio = document.getElementById('laylaAudio')
+const layla = document.getElementById('layla')
+
+layla.addEventListener("click", () => {
+    laylaAudio.play()
+})
