bf8f635dec08dcec6fe1ec3591ac5641c0d4c148
Eduardo Marin <du.marin10@gmail.com>
2023-04-20 23:13:43 -0300

    1.0.0

--- code/memberAccess.js
+++ code/memberAccess.js
@@ -1,9 +1,21 @@
+// Se você encontrou essa pasta, então é um desenvolvedor, se junte a nós nessa parceria
+//Encaminhe uma solicitação para conhecer os benefícios.
 
 function memberAccess() {
     let userId = document.getElementById('userId').value
     let userPassword = document.getElementById('userPassword').value
 
-    if(userId === "mayara" && userPassword === "123") {
+    if(userId === "M@ster_01!" && userPassword === "#io!47") {
         window.location.href = "/index.html"
+    }
+    else if(userId === "Root!!002" && userPassword === "PK_012@") {
+        window.location.href = "/index.html"
+    }
+    else if(userId === "masterdev" && userPassword === "dev@202Dig#") {
+        window.location.href = "/index.html"
+    }
+    else {
+        alert("Acesso negado\nEntre em contato com o suporte técnico")
+        
     }
 }
