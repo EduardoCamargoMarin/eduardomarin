b4fc4a0940dd048c2a210d4c73f5047ea6be15a4
Eduardo Marin <du.marin10@gmail.com>
2023-04-22 13:28:07 -0300

    1.0.0

--- code/memberAccess.js
+++ code/memberAccess.js
@@ -4,18 +4,7 @@
 function memberAccess() {
     let userId = document.getElementById('userId').value
     let userPassword = document.getElementById('userPassword').value
+    let access = document.getElementById('btnAccess')
 
-    if(userId === "M@ster_01!" && userPassword === "#io!47") {
-        window.location.href = "/index.html"
-    }
-    else if(userId === "Root!!002" && userPassword === "PK_012@") {
-        window.location.href = "/index.html"
-    }
-    else if(userId === "masterdev" && userPassword === "dev@202Dig#") {
-        window.location.href = "/index.html"
-    }
-    else {
-        alert("Acesso negado\nEntre em contato com o suporte técnico")
-        
-    }
+  
 }
