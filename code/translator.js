bd4eb39027fa202baa84db513bf00455aa2604f6
Eduardo Marin <du.marin10@gmail.com>
2023-04-30 21:01:06 -0300

    1.0.0

--- code/translator.js
+++ code/translator.js
@@ -30,6 +30,7 @@ function changeLanguage() {
 
     const profissional = document.getElementById('profissional')
     const profissioanlTexto = document.getElementById('profissioanlTexto')
+    const hub = document.getElementById('hub')
 
     const academico = document.getElementById('academico')
     const academicoTexto = document.getElementById('academicoTexto')
@@ -47,6 +48,22 @@ function changeLanguage() {
     const contratoTexto = document.getElementById('contratoTexto')
     const sisterTexto = document.getElementById('sisterTexto')
     const parceiroTexto = document.getElementById('parceiroTexto')
+
+    //offcanvas EVA
+    const evaHeader = document.getElementById('evaHeader')
+    const evaBody = document.getElementById('evaBody')
+    const evaQuestion01 = document.getElementById('evaQuestion01')
+    const evaQuestion02 = document.getElementById('evaQuestion02')
+    const evaQuestion03 = document.getElementById('evaQuestion03')
+    const evaQuestion04 = document.getElementById('evaQuestion04')
+
+    const evaAnswer01 = document.getElementById('evaAnswer01')
+    const evaAnswer02 = document.getElementById('evaAnswer02')
+    const evaAnswer03 = document.getElementById('evaAnswer03')
+    const evaAnswer04 = document.getElementById('evaAnswer04')
+
+    
+    
 
     if (selectedLanguage === "en") {
         about.innerText = 'About'
@@ -66,7 +83,8 @@ function changeLanguage() {
         
         profissional.innerText = 'Professional Career'
         profissioanlTexto.innerText = 'Degree in Production Engineering and professional in Web Development, driven by the adrenaline of creating and developing projects.\n\nHis studies include the implementation of intelligent systems to enhance user experience and website design.\n\nIt`s not hard to find Eduardo, as you may find a rubber duck on his work desk, his companion in application debugging.'
-    
+        hub.innerText = 'News HUB'
+
         academico.innerText = 'Academic Career'
         academicoTexto.innerText = 'Author of two scientific articles published in the interdisciplinary journal RECIMA21 with a special participation in the National Congress of Scientific Initiation (CONIC) on the distribution of photovoltaic solar energy for electric vehicles.\n\nThe calculations were analyzed for the best strategy of panel installation, ensuring that the system can be implemented in any region of the world.\n\nThe project was based on Production Engineering and Electrical Engineering.'
     
@@ -91,8 +109,18 @@ function changeLanguage() {
         btnSister.innerText = 'Project SISTERS'
         btnService.innerText = 'Contract for services'
         btnPartner.innerText = 'Become a partner'
-    
-    } 
+
+        evaHeader.innerText = 'Virtual Assistant - EVA'
+        evaBody.innerText = '"I am an artificial intelligence called EVA. I am being developed by my creator.\n However, I can provide limited information for you. What information are you looking for?"'
+        evaQuestion01 = 'Who are you?'
+        evaQuestion02.innerText = 'Who is your creator?'
+        evaQuestion03.innerText = 'I need a website tour'
+        evaQuestion04.innerHTML = 'Project SISTERS'
+        evaAnswer01.innerText = 'I am EVA, I was developed to assist travelers on their journeys to discover their goals.\nI was deployed in this environment to understand the needs of new travelers so that I can enhance my skills as an AI.\nI hope we can become partners during this journey, because I am very pleased to meet you.'
+        evaAnswer02.innerText = 'My creator goes by the name of Eduardo Marin. He is a web developer and an enthusiast in AI. His main focus is to develop new websites, improve his skills in implementing AI, and creating new prototypes.'
+        evaAnswer03.innerText = 'As you wish, traveler.\nOn this website, you can choose to browse or purchase website templates for your personal or professional goals.\nNot only that, but you can also choose to acquire the creator`s skills as a developer to assist you in your projects.\nAnd finally, you can connect with us by becoming a partner.\nAll this information you can find during your visit to our site. I hope you have a great experience. We`ll be here if you need us.'
+        evaAnswer04.innerText = 'I am the first prototype of an AI called EVA.\nI am the model that will raise new sisters. To ensure our communication with all travelers and their needs.\nEach of the sisters will have a quality and ability, and even a name. This will be available in the near future.\nI hope I can stay a little longer with us. So you can witness the birth of the other sisters. It will be magnificent.'
+    }
     else if (selectedLanguage === "pt") {
         about.innerText = 'Quem sou'
         project.innerText = 'Projeto'
@@ -111,7 +139,8 @@ function changeLanguage() {
         
         profissional.innerText = 'Profissional'
         profissioanlTexto.innerText = 'Formação em Engenharia de Produção e profissional em Desenvolvimento Web, movido pela adrenalina da criação e desenvolvimento de projetos.\n\nSeus estudos englobam a implementação de sistemas inteligentes para aprimorar a experiência do usuário e no design de websites.\n\nNão é difícil encontrar o Eduardo, pois em sua mesa de trabalho é capaz que você encontre um pato de borracha, o seu companheiro em debbug de aplicações.'
-    
+        hub.innerText = 'Hub de noticias'
+
         academico.innerText = 'Acadêmico'
         academicoTexto.innerText = 'Autor de dois artigos científicos publicados na revista interdisciplinar RECIMA21 com participação especial no Congresso Nacional de Iniciação Científica (CONIC) sobre a distribuição de energia solar fotovoltaica para veículos elétricos.\n\nOs calculos foram análisados sobre a melhor estratégia da instalação das placas, garantindo que o sistema possa ser implantada em qualquer região do mundo.\n\nO projeto teve o seu embasamento aplicado na Engenharia de Produção e Engenharia Elétrica.'
     
@@ -137,5 +166,16 @@ function changeLanguage() {
         btnSister.innerText = 'Projeto SISTERS'
         btnService.innerText = 'Contratação para serviços'
         btnPartner.innerText = 'Seja um parceiro'
+
+        evaHeader.innerText = 'EVA - Assistente Virtual'
+        evaBody.innerText = 'Eu sou uma inteligência artificial chamada EVA. Eu estou em desenvolvimento pelo meu criador.\nMas posso providênciar informações limitadas para você. Qual informação você procura?'
+        evaQuestion01 = 'Quem é você?'
+        evaQuestion02.innerText = 'Quem é o seu criador?'
+        evaQuestion03.innerText = 'Preciso de um tour pelo site'
+        evaQuestion04.innerHTML = 'Projeto SISTERS'
+        evaAnswer01.innerText = 'Eu sou a EVA, eu fui desenvolvida para auxíliar viajantes em suas jornadas para descobrirem os seus objetivos.\nEu fui implantada nesse ambiente para entender as necessidades novos viajantes, para que eu possa aprimorar minhas habilidades como uma IA.\nEu espero que possamos nos tornar parceiros durante essa jornada, porquê eu estou muito contente em te conhecer.'
+        evaAnswer02.innerText = 'Meu criador tem a identificação de Eduardo Marin. Ele é um desenvolvedor web e um entusiasta em IA. o sistema principal dele consiste em desenvolver novos websites, aprimorar seus conhecimentos em implatação de IA e desenvolvedor novos protótipos.'
+        evaAnswer03.innerText = 'Como quiser viajante.\nNesse website, você escolher entre navegar ou comprar templates para websites para os seus objetivos pessoais ou profissionais.\nNão só isso, como também, você pode escolher adquirir as habilidades do criador como um desenvolvedor para auxíliar você em seus projetos.\nE finalmente, você pode se conectar com nós, se tornando um parceiro.\nToda essa informação você consegue encontrar durante a sua visita pelo nosso site. Eu espero que você tenha uma ótimo experiência. Nós estaremos aqui se precisar..'
+        evaAnswer04.innerText = 'Eu sou o primeiro protótipo de IA chamada EVA.\nEu sou a modelo que vai erguer novas irmãs. Para garantir nossa comunicação com todos os viajantes e suas necessidades.\nCada uma das irmãs terá uma qualidade e habilidade e até mesmo um nome. Isso estará disponível em um futuro próximo.\nEspero que posso ficar um pouco a mais conosco. Para que você possa presenciar o nascimento das outras irmãs. Será magnifico!'
     }
   }
