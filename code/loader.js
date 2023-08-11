b998c10faba2428d5ead44b97710c7e9098a036d
Eduardo Marin <du.marin10@gmail.com>
2023-08-11 00:01:52 -0300

    1.0.4.1

--- code/loader.js
+++ code/loader.js
@@ -3,21 +3,21 @@ window.addEventListener("load", () => {
 
     popoverLoading.textContent = "Carregando em segundo plano..."
     popoverLoading.style.color = "#f7f7f7"
+    popoverLoading.style.zIndex = '100'
     
     let popoverLoadingContainer = document.getElementById('secondLoader')
     
     popoverLoadingContainer.appendChild(popoverLoading)
-    
-
-        const loader = document.getElementById('abc')
-           loader.style.display = 'none'
-           popoverLoading.style.display = 'none'
- 
-    
-
         const secondLoader = document.getElementById('secondLoader')
         secondLoader.style.display = 'none'
     }
 )
+
+setTimeout(() => {
+    const loader = document.getElementById('abc')
+    loader.style.display = 'none'
+    popoverLoading.style.display = 'none' 
+}, 1000);
+
 
 
